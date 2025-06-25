b03bbd959ecaae41974e4b6018d862770da967df
Gust4av0 <gustavomarcolin2005@gmail.com>
2025-06-25 14:22:07 -0300

    teste

--- Projeto/frontend/src/pages/Cadastro.tsx
+++ Projeto/frontend/src/pages/Cadastro.tsx
@@ -63,7 +63,7 @@ function Cadastro() {
       setError("As senhas não coincidem!");
       return;
     }
-      // ssassfffffffffxxs
+      // ssassfffff
     try {
       const response = await axios.post("https://alugaaize.local/api/usuarios", {
         nome: name,
